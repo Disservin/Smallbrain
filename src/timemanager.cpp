e5454fee4a371aa94088f24ecff88a161f95598b
disservin <45608332+Disservin@users.noreply.github.com>
2022-05-26 23:43:22 +0200

    Update timemanager.cpp
    Bench: 2150799

--- src/timemanager.cpp
+++ src/timemanager.cpp
@@ -1,19 +1,18 @@
 #include "timemanager.h"
 
 Time optimumTime(int64_t avaiableTime, int inc, int ply, int mtg) {
-    float scale = 0.1f/(log(ply+2));
-    int64_t optimum = (avaiableTime + inc / 2) * scale;
-    int64_t maximum = avaiableTime * 0.7f;
-    if (optimum <= 0.02f * avaiableTime) {
-        int denom = mtg > 0 ? mtg : 20;
-        optimum = avaiableTime / denom;
-        maximum = optimum;
-    }
-    optimum = std::clamp(optimum, 1LL, avaiableTime);
-    maximum = std::clamp(maximum, 1LL, avaiableTime);
-    
     Time time;
-    time.optimum = optimum;
-    time.maximum = maximum;
+    if (mtg > 0) {
+        time.optimum = avaiableTime / mtg;
+    }
+    else {
+        time.optimum = avaiableTime / 20;
+    }
+    time.optimum += inc / 2;
+    if (time.optimum >= avaiableTime) {
+        time.optimum = std::clamp(time.optimum, 1LL, avaiableTime / 20);
+        time.maximum = time.optimum;
+    }
+
     return time;
 }
