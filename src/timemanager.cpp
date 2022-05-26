bbb2cdb4d9e5fe158df844ef19e71face3258b3e
disservin <45608332+Disservin@users.noreply.github.com>
2022-05-26 23:47:38 +0200

    Update timemanager.cpp
    Bench: 2150799

--- src/timemanager.cpp
+++ src/timemanager.cpp
@@ -2,6 +2,7 @@
 
 Time optimumTime(int64_t avaiableTime, int inc, int ply, int mtg) {
     Time time;
+    time.optimum = avaiableTime / 20;
     if (mtg > 0) {
         time.optimum = avaiableTime / mtg;
     }
