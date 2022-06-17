5a5b7e43e6a40ff9facb639d6cd05b4786153106
disservin <45608332+Disservin@users.noreply.github.com>
2022-06-17 20:38:05 +0200

    Update search.cpp
    Bench: 1554016

--- src/search.cpp
+++ src/search.cpp
@@ -282,7 +282,7 @@ int Search::absearch(int depth, int alpha, int beta, int ply, Stack *ss) {
         // late move pruning/movecount pruning
         if (!RootNode && !PvNode
             && !capture && !inCheck && !givesCheck
-            && quietMoves.size > (4 + depth * depth) / (2 - improving)
+            && quietMoves.size > (4 + depth * depth) / 2
             && !move.promoted()) {
             board.unmakeMove(move);
             continue;
