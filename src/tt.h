c7d212af49acb3161c4873d106df196a0891565d
disservin <45608332+Disservin@users.noreply.github.com>
2022-06-22 19:25:22 +0200

    Update tt.h
    Bench: 662315

--- src/tt.h
+++ src/tt.h
@@ -1,11 +1,11 @@
 #pragma once
 #include "types.h"
 
-struct TEntry {
+struct __attribute__((__packed__)) TEntry {
 	U64 key;
+	int score;
+	uint16_t move;
+	uint16_t age;
 	uint8_t depth;
 	uint8_t flag;
-	uint16_t move;
-	int score;
-	uint16_t age;
 };
