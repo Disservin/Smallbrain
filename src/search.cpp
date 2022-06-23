71665e34e0205f08265b5b0ba61c51961cbd280d
disservin <45608332+Disservin@users.noreply.github.com>
2022-06-23 20:58:24 +0200

    Update search.cpp
    Bench: 684147

--- src/search.cpp
+++ src/search.cpp
@@ -242,7 +242,7 @@ int Search::absearch(int depth, int alpha, int beta, int ply, Stack *ss) {
     }
 
     // Internal iterative reduction based on Rebel's idea
-    if (!ttMove && depth >= 6)
+    if (!ttMove && depth >= 10)
         depth --;
 
     Movelist ml = board.legalmoves();
