8724ca9f45c5409cfff62cfa04f72624f34138f0
disservin <45608332+Disservin@users.noreply.github.com>
2022-06-24 12:21:22 +0200

    fix
    Bench: 732970

--- src/neuralnet.cpp
+++ src/neuralnet.cpp
@@ -73,5 +73,5 @@ int32_t NNUE::output() {
         output += relu(accumulator[i]) * hiddenWeights[i];
     }
     output += outputBias[0];
-    return output / (64 * 256);
+    return output / (64 * 512);
 }
