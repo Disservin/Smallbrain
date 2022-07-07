e05c1df008128911b80102cce0c18088db562398
disservin <45608332+Disservin@users.noreply.github.com>
2022-07-07 16:00:55 +0200

    Update search.cpp
    Bench: 5023311

--- src/search.cpp
+++ src/search.cpp
@@ -214,7 +214,7 @@ Score Search::absearch(int depth, Score alpha, Score beta, Stack *ss, ThreadData
         // bool givesCheck = td->board.isSquareAttacked(color, td->board.KingSQ(~color));
 
         // late move reduction
-        if (depth >= 3 && !inCheck && madeMoves > 3 + 2 * PvNode) {
+        if (depth >= 3 && !inCheck && madeMoves > 2 + 2 * PvNode) {
             int rdepth = reductions[madeMoves][depth];
             rdepth -= td->id % 2;
             rdepth = std::clamp(newDepth - rdepth, 1, newDepth + 1);
