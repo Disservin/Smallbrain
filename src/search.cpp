8711745e8bff63d48da761a98c8fcaaeead350cf
disservin <45608332+Disservin@users.noreply.github.com>
2022-07-08 21:19:50 +0200

    Update search.cpp
    Bench: 5483382

--- src/search.cpp
+++ src/search.cpp
@@ -359,7 +359,7 @@ void Search::iterative_deepening(int search_depth, uint64_t maxN, Time time, int
                 searchTime = startTime * 1.05f;
             }
 
-            if (elapsed() > searchTime * 0.8) break;
+            if (elapsed() > maxTime * 0.8) break;
         }
 
     }
