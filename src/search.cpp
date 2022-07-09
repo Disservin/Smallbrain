678f79a2bf4bf2d94da25f733f134c80c6b772bc
disservin <45608332+Disservin@users.noreply.github.com>
2022-07-09 14:27:11 +0200

    Update search.cpp
    Bench: 5483382

--- src/search.cpp
+++ src/search.cpp
@@ -361,7 +361,8 @@ void Search::iterative_deepening(int search_depth, uint64_t maxN, Time time, int
             if (adjustedTime && td->pv_table[0][0] != reducedTimeMove)
                 searchTime = startTime * 1.05f;
 
-            if (ms + iterationTime * 2 > maxTime) break;
+            long long nextTime = ms + iterationTime;
+            if (nextTime >= searchTime || nextTime >= maxTime) break;
         }
     }
 
