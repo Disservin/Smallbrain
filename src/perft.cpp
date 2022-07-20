b736cc32016c1fb62e0a31b22669470370972be8
disservin <45608332+Disservin@users.noreply.github.com>
2022-07-20 11:24:32 +0200

    Update search.cpp
    Bench: 3031886

--- src/perft.cpp
+++ src/perft.cpp
@@ -17,7 +17,7 @@ void Perft::testAllPos() {
                             "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - ",
                             "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - ",
                             "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",
-                            "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8  ",
+                            "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",
                             "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10 "};
     int depths[] = {6, 5, 7, 6, 5, 5};
     U64 expected[] = {119060324, 193690690, 178633661, 706045033, 89941194, 164075551};
