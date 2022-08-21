17715b56f4b48e970f78fcfd271d0b67a747de88
disservin <45608332+Disservin@users.noreply.github.com>
2022-08-21 21:40:27 +0200

    Update timemanager.cpp

--- src/timemanager.cpp
+++ src/timemanager.cpp
@@ -6,7 +6,7 @@ Time optimumTime(int64_t avaiableTime, int inc, int ply, int mtg)
     time.optimum = avaiableTime / 20;
 
     mtg = mtg == 0 ? 20 : mtg;
-    int overhead = avaiableTime < 200 ? (avaiableTime < 100 ? 5 : 0) : 10;
+    int overhead = avaiableTime < 200 ? 5 : 10;
 
     avaiableTime -= overhead;
     time.optimum = avaiableTime / mtg;
