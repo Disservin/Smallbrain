0267eb59aafc59e2b685fc40b955cb809075e93f
disservin <disservin.social@gmail.com>
2022-09-24 19:17:06 +0200

    small time mang. changes (#188)
    Bench: 2505053

--- src/timemanager.cpp
+++ src/timemanager.cpp
@@ -1,23 +1,22 @@
 #include "timemanager.h"
 
-Time optimumTime(int64_t avaiableTime, int inc, int ply, int mtg)
+Time optimumTime(int64_t avaiableTime, int inc, int ply, double mtg)
 {
     Time time;
-    time.optimum = avaiableTime / 20;
 
-    mtg = mtg == 0 ? 20 : mtg;
-    int overhead = avaiableTime < 200 ? (avaiableTime < 100 ? 5 : 0) : 10;
+    mtg = mtg == 0 ? 20.0 : mtg;
 
+    int overhead = avaiableTime < 200 ? (avaiableTime < 100 ? 0 : 5) : 10;
     avaiableTime -= overhead;
-    time.optimum = avaiableTime / mtg;
 
+    time.optimum = (int64_t)(avaiableTime / mtg);
     time.optimum += inc / 2;
     if (time.optimum >= avaiableTime)
     {
-        time.optimum = std::clamp(time.optimum, (int64_t)1, avaiableTime / 20);
+        time.optimum = (int64_t)std::max(1.0, avaiableTime / 20.0);
     }
 
-    time.maximum = time.optimum * 1.05f;
+    time.maximum = (int64_t)(time.optimum * 1.05);
     if (time.maximum >= avaiableTime)
     {
         time.maximum = time.optimum;
