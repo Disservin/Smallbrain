b4060083ef16e3a351b11b8956fd1640b65b2641
disservin <45608332+Disservin@users.noreply.github.com>
2022-10-22 10:30:38 +0200

    Update timemanager.cpp
    Bench: 3124331

--- src/timemanager.cpp
+++ src/timemanager.cpp
@@ -9,7 +9,7 @@ Time optimumTime(int64_t avaiableTime, int inc, int ply, int mtg)
 
     if (mtg == 0)
     {
-        mtg = 20;
+        mtg = 50;
         overhead = std::max(10 * (50 - ply / 2), 0);
     }
     else
