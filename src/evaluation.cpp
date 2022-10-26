df2396aca14255c11930699b733a8ceb0110924b
disservin <45608332+Disservin@users.noreply.github.com>
2022-10-26 22:47:58 +0200

    Update evaluation.cpp
    Bench: 3302746

--- src/evaluation.cpp
+++ src/evaluation.cpp
@@ -8,9 +8,8 @@ Score evaluation(Board &board)
 {
     int32_t v = NNUE::output(board.getAccumulator()) * (board.sideToMove * -2 + 1);
 
-    v = static_cast<float>(v) * (1 - (board.halfMoveClock / 1000.0));
-    v = static_cast<int>(v);
-    Score score = std::clamp(v, (int32_t)(VALUE_MATED_IN_PLY + 1), (int32_t)(VALUE_MATE_IN_PLY - 1));
+    v = static_cast<double>(v) * (1.0 - (board.halfMoveClock / 1000.0));
+    Score score = std::clamp(static_cast<int>(v), (int32_t)(VALUE_MATED_IN_PLY + 1), (int32_t)(VALUE_MATE_IN_PLY - 1));
     return score;
 }
 } // namespace Eval
