37590d162feda6e66ea046bf578c5e9f9569c214
disservin <45608332+Disservin@users.noreply.github.com>
2022-11-25 18:29:15 +0100

    Update ucioptions.cpp
    Bench: 3077738

--- src/ucioptions.cpp
+++ src/ucioptions.cpp
@@ -3,7 +3,7 @@
 std::vector<optionType> optionsPrint{
     optionType("Hash", "spin", "400", "1", "57344"), optionType("EvalFile", "string", "default.nnue", "", ""),
     optionType("Threads", "spin", "1", "1", "256"), optionType("SyzygyPath", "string", "<empty>", "", ""),
-    optionType("Chess960", "string", "false", "false", "true")};
+    optionType("UCI_Chess960", "check", "false", "", "")};
 
 void uciOptions::printOptions()
 {
